98370594e57e67561ccfeec083fadbb18ffe377f
kf <kf>
2017-12-07 16:44:23 -0600

    navigation selectable

--- src/navigation.js
+++ src/navigation.js
@@ -9,14 +9,20 @@ class Navigation extends Component {
     	super(props);
     	this.state = {selectedTab: 'Timeline'};
     }
-
+	handleClick(obj,e) {
+		console.log(obj);
+		// console.log('this is:', this);
+		this.setState({
+			selectedTab: obj
+		});
+	}
 	render() {
 		return (
 		    <div className="nagivation">
 	        	<nav>
     		        {navItems.map(obj => {
 			          	return (
-			            	<div className={"navigation-item " + (this.state.selectedTab == obj ? 'active' : '')} id={obj}>
+			            	<div className={"navigation-item " + (this.state.selectedTab == obj ? 'active' : '')} id={obj} onClick={(e) => this.handleClick(obj, e)}>
 				            	<div className="icon-contain">
 				            		icon
 				              	</div>
