26931304cbeed6271c89c00dc51411853d79469c
kf <kf>
2018-01-02 17:43:09 -0600

    profile banner images fixed

--- src/js/profile.js
+++ src/js/profile.js
@@ -129,7 +129,7 @@ class Profile extends Component {
     }
 
     let bannerStyle = {
-      background: 'url(' +  (profile.profile_use_background_image ? profile.profile_background_image_url_https : profile.profile_banner_url) + ') no-repeat 50% 50% / cover',
+      background: 'url(' + profile.profile_banner_url + ') no-repeat 50% 50% / cover',
     };
 
     return (
